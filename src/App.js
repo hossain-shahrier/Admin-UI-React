6920d845612d9dc7c4a08c623f2181a642592363
hossain-shahrier <hossainshahrier.shm@gmail.com>
2022-05-15 18:53:46 +0600

    Featured Info, Chart created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import "./app.css";
 import Topbar from "./components/topbar/Topbar";
 import Sidebar from "./components/sidebar/Sidebar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Topbar />
       <div className="container">
         <Sidebar />
+        <Home />
       </div>
-    </div>
+    </Router>
   );
 }
 
---
+++ src/dummyData.js
@@ -0,0 +1,54 @@
+export const userData = [
+  {
+    name: "Jan",
+    "Active User": 4570,
+  },
+  {
+    name: "Feb",
+    "Active User": 5420,
+  },
+  {
+    name: "Page C",
+    "Active User": 2479,
+  },
+  {
+    name: "Mar",
+    "Active User": 8780,
+  },
+  {
+    name: "Apr",
+    "Active User": 5200,
+  },
+  {
+    name: "May",
+    "Active User": 5600,
+  },
+  {
+    name: "Jun",
+    "Active User": 8005,
+  },
+  {
+    name: "Jul",
+    "Active User": 7800,
+  },
+  {
+    name: "Aug",
+    "Active User": 4800,
+  },
+  {
+    name: "Sep",
+    "Active User": 6800,
+  },
+  {
+    name: "Oct",
+    "Active User": 4250,
+  },
+  {
+    name: "Nov",
+    "Active User": 7500,
+  },
+  {
+    name: "Dec",
+    "Active User": 3540,
+  },
+];
